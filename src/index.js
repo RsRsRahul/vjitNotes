3614600094a87adf216b706d4f0be3f5c9abe62b
rahul <rsrsrahul959@gmail.com>
2022-08-01 23:07:06 +0530

    adjusted the size of the side bar

--- src/index.js
+++ src/index.js
@@ -3,16 +3,24 @@ import ReactDOM from 'react-dom/client';
 import DataDisplay from './dataDisplay';
 import SideBar from './sideBar';
 import { Grid } from '@mui/material';
+import NavBar from './navBar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <>
+    <Grid>
+      <Grid item xs={12}>
+        <NavBar/>
+      </Grid>
+    </Grid>
     <Grid container direction="row">
-     <Grid item lg={1} xl={1} style={{border:"5px black",height:"100vh" }}>
+     <Grid item lg={2} xl={2} style={{border:"5px black",height:"100vh" }}>
       <SideBar />
      </Grid>
      <Grid item>
       <DataDisplay/>
      </Grid>
     </Grid>
+  </>
 );
 
--- src/sideBar.js
+++ src/sideBar.js
@@ -31,60 +31,60 @@ export default function SideBar() {
       component="nav"
       aria-labelledby="nested-list-subheader"
       subheader={
-        <ListSubheader component="div" id="nested-list-subheader">
+        <ListSubheader component="div" id="nested-list-subheader" style={{textAlign:"center"}}>
          Years
         </ListSubheader>
       }
     >
       <ListItemButton onClick={handleClickYear1}>
-        <ListItemText primary="1st YEAR" />
+        <ListItemText primary="1st YEAR" style={{paddingLeft:"1rem"}}/>
       </ListItemButton>
       <Collapse in={open1} timeout="auto" unmountOnExit>
         <List component="div" disablePadding>
           <ListItemButton sx={{ pl: 4 }}>
-            <ListItemText primary="SEM 1" />
+            <ListItemText primary="SEM 1" style={{paddingLeft:"1rem"}}/>
           </ListItemButton>
           <ListItemButton sx={{ pl: 4 }}>
-            <ListItemText primary="SEM 2" />
+            <ListItemText primary="SEM 2" style={{paddingLeft:"1rem"}}/>
           </ListItemButton>
         </List>
       </Collapse>
       <ListItemButton onClick={handleClickYear2}>
-        <ListItemText primary="2nd YEAR" />
+        <ListItemText primary="2nd YEAR" style={{paddingLeft:"1rem"}}/>
       </ListItemButton>
       <Collapse in={open2} timeout="auto" unmountOnExit>
         <List component="div" disablePadding>
           <ListItemButton sx={{ pl: 4 }}>
-            <ListItemText primary="SEM 3" />
+            <ListItemText primary="SEM 3" style={{paddingLeft:"1rem"}}/>
           </ListItemButton>
           <ListItemButton sx={{ pl: 4 }}>
-            <ListItemText primary="SEM 4" />
+            <ListItemText primary="SEM 4" style={{paddingLeft:"1rem"}}/>
           </ListItemButton>
         </List>
       </Collapse>
       <ListItemButton onClick={handleClickYear3}>
-        <ListItemText primary="3rd YEAR" />
+        <ListItemText primary="3rd YEAR" style={{paddingLeft:"1rem"}}/>
       </ListItemButton>
       <Collapse in={open3} timeout="auto" unmountOnExit>
         <List component="div" disablePadding>
-          <ListItemButton sx={{ pl: 4 }}>
-            <ListItemText primary="SEM 5" />
+          <ListItemButton sx={{ pl: 4 }} >
+            <ListItemText primary="SEM 5" style={{paddingLeft:"1rem"}}/>
           </ListItemButton>
           <ListItemButton sx={{ pl: 4 }}>
-            <ListItemText primary="SEM 6" />
+            <ListItemText primary="SEM 6" style={{paddingLeft:"1rem"}}/>
           </ListItemButton>
         </List>
       </Collapse>
       <ListItemButton onClick={handleClickYear4}>
-        <ListItemText primary="4th YEAR" />
+        <ListItemText primary="4th YEAR" style={{paddingLeft:"1rem"}}/>
       </ListItemButton>
       <Collapse in={open4} timeout="auto" unmountOnExit>
         <List component="div" disablePadding>
           <ListItemButton sx={{ pl: 4 }}>
-            <ListItemText primary="SEM 7" />
+            <ListItemText primary="SEM 7" style={{paddingLeft:"1rem"}}/>
           </ListItemButton>
-          <ListItemButton sx={{ pl: 4 }}>
-            <ListItemText primary="SEM 8" />
+          <ListItemButton sx={{ pl: 4 }} >
+            <ListItemText primary="SEM 8" style={{paddingLeft:"1rem"}}/>
           </ListItemButton>
         </List>
       </Collapse>
