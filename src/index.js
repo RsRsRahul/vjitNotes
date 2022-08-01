38b1f3b553b536ef643cc634158237f0369e5141
rahul <rsrsrahul959@gmail.com>
2022-08-01 22:34:08 +0530

    done with the sideBar

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import { Grid } from '@mui/material';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Grid direction="column">
-     <Grid item lg={1} style={{border:"5px black dotted",height:"100vh" }}>
+    <Grid container direction="row">
+     <Grid item lg={1} xl={1} style={{border:"5px black",height:"100vh" }}>
       <SideBar />
      </Grid>
      <Grid item>
