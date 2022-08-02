1437e84a8e4ae0760d0d2ca83d365116215e1a9e
rahul <rsrsrahul959@gmail.com>
2022-08-02 19:34:05 +0530

    added the data

---
+++ data/data.js
@@ -0,0 +1,78 @@
+export const data =[
+    {
+        "id":1,
+        "subjects":[
+            "BASIC ELECTIRCAL ENGINEERING",
+            "CHEMISTRY",
+            "PPS-I",
+            "M-I",
+            "ENGLISH"
+        ]
+    },
+    {
+        "id":2,
+        "subjects":[
+            "APPLIED PHYSICS",
+            "PPS-II",
+            "ELS",
+            "M-II",
+            "EGM"
+        ]
+    },
+    {
+        "id":3,
+        "subjects":[
+            "P&S",
+            "EDC",
+            "DLD",
+            "MFCS",
+            "DATA STRUCTURES",
+            "PYTHON",
+            "GENDER SENSITIZATION"
+        ]
+    },
+    {
+        "id":4,
+        "subjects":[
+            "DAA",
+            "COMPUTER ORGANIZATION",
+            "SOFTWARE ENGINEERING",
+            "DBMS",
+            "JAVA",
+            "ENVIRONMENTAL SCIENCE",
+            "PROFESSIONAL COMMUNICATION"
+        ]
+    },
+    {
+        "id":5,
+        "subjects":[
+            "FLAT",
+            "LINUX PROGRAMMING",
+            "WEB TECHNOLOGIES",
+            "OPERATING SYSTEM",
+            "COMPUTER NETWORKS",
+            "PDBS"
+        ]
+    },
+    {
+        "id":6,
+        "subjects":[
+            "ARTIFICIAL INTELLIGENCE",
+            "MEFA",
+            "COMPUTER DESIGN",
+            "DWDM",
+            "QLMR",
+            "ACS",
+            "SWSM"
+
+        ]
+    },
+    {
+        "id":7,
+        "subjects":[
+            "MAD",
+            "CLOUD COMPUTING",
+            "E-COMMERCE"
+        ]
+    }
+]
